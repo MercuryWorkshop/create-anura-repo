aa64e812dab41a75989d06bd21ca0cb766e9e61d
Percs <83934299+Percslol@users.noreply.github.com>
2024-02-27 21:48:44 -0600

    1.0.0

---
+++ index.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+import inquirer from 'inquirer';
+
+(async () => {
+  const manifestData = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'maintainerName',
+      message: 'Enter maintainer name:'
+    },
+    {
+      type: 'input',
+      name: 'maintainerEmail',
+      message: 'Enter maintainer email:'
+    },
+    {
+      type: 'input',
+      name: 'maintainerWebsite',
+      message: 'Enter maintainer website:'
+    }
+  ]);
+
+  const version = "2.0.0";
+  const result = { apps: [], libs: [] };
+
+  const readManifest = (directory) => {
+    const manifestPath = path.join(directory, 'manifest.json');
+    if (fs.existsSync(manifestPath)) {
+      return JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
+    } else {
+      return null;
+    }
+  };
+
+  const readDirectories = (dirPath, result) => {
+    const files = fs.readdirSync(dirPath);
+
+    files.forEach(file => {
+      const filePath = path.join(dirPath, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        const manifest = readManifest(filePath);
+        if (manifest) {
+          result.push(manifest);
+        } else {
+          readDirectories(filePath, result);
+        }
+      }
+    });
+  };
+
+  readDirectories(path.join(new URL(import.meta.url).pathname, '../apps'), result.apps);
+  readDirectories(path.join(new URL(import.meta.url).pathname, '../libs'), result.libs);
+
+  const listJSON = JSON.stringify(result, null, 2);
+  fs.writeFileSync('list.json', listJSON);
+
+  const manifestJSON = JSON.stringify({
+    maintainer: {
+      name: manifestData.maintainerName,
+      email: manifestData.maintainerEmail,
+      website: manifestData.maintainerWebsite
+    },
+    version
+  }, null, 2);
+  fs.writeFileSync('manifest.json', manifestJSON);
+
+  console.log('Anura repo successfully created!');
+})();
